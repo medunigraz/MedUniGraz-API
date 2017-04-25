898d9c7d9e2271b8189a8f474a8b40f126f35e13
Gerhard Janisch <Gerhard.Janisch@medunigraz.at>
2017-04-25 10:14:20 +0200

    New application Modes

--- medunimaps/src/app/base/applicationmode.ts
+++ medunimaps/src/app/base/applicationmode.ts
@@ -1,27 +1,28 @@
 
 export enum ApplicationModeT {
   DEFAULT,
-  EDIT_NODES
+  EDIT_NODES,
+  EDIT_POIS,
+  EDIT_BACKGROUND
 }
 
 export class ApplicationMode {
 
   constructor(
     public mode: ApplicationModeT,
-    public name: string,
-    public showDemoRoute: boolean
+    public name: string
   )
   { }
 
   static CreateDefault(): ApplicationMode {
     //return new ApplicationMode(ApplicationModeT.DEFAULT, "Default", true);
-    return new ApplicationMode(ApplicationModeT.EDIT_NODES, "Edit Nodes", false)
+    return new ApplicationMode(ApplicationModeT.EDIT_NODES, "Navigationsmesh");
   }
 
 }
 
 export const ApplicationModes = [
   ApplicationMode.CreateDefault(),
-  //new ApplicationMode(ApplicationModeT.EDIT_NODES, "Edit Nodes", false)
-
+  new ApplicationMode(ApplicationModeT.EDIT_POIS, "Points of Interest"),
+  new ApplicationMode(ApplicationModeT.EDIT_BACKGROUND, "Hintergrund")
 ]
